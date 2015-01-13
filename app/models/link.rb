985fe6532a0709a26382cdd7f4f9ee60667b4546
Roy McKenzie <roymckenzie@gmail.com>
2015-01-12 23:35:23 -0800

    Added Paperclip attachments to Link and Thought models. Added process_link method to Link model.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,15 @@
 class Link < ActiveRecord::Base
+  has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100>" }, :default_url => "https://placehold.it/300x300"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  before_save :process_link
+
+  def process_link
+    og           = OpenGraph.new(self.url)
+    self.url     = og.url
+    self.title   = og.title
+    self.descr   = og.description
+    if og.images.any?
+      self.image = open(og.images[0])
+    end
+  end
 end
