7dbc6a737a0798d51926377f8957bdb7ac3aa31d
Roy McKenzie <roymckenzie@gmail.com>
2015-01-12 22:53:57 -0800

    Migrate Thought and Link models.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113063020) do
+ActiveRecord::Schema.define(version: 20150113065216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "links", force: :cascade do |t|
+    t.string   "url"
+    t.string   "title"
+    t.text     "descr"
+    t.string   "site_name"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  add_index "links", ["url"], name: "index_links_on_url", unique: true, using: :btree
+
+  create_table "thoughts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "link_id"
+    t.datetime "published"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  add_index "thoughts", ["link_id"], name: "index_thoughts_on_link_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,4 +64,5 @@ ActiveRecord::Schema.define(version: 20150113063020) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "thoughts", "links"
 end
