4ede0c681532f51b3c2d2079874779c9113637ec
Roy McKenzie <roymckenzie@gmail.com>
2015-01-13 22:27:07 -0800

    Updated Thought image thumbnail creation options.

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,5 +1,5 @@
 class Thought < ActiveRecord::Base
   belongs_to :link
-  has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100#" }, :default_url => "https://placehold.it/300x300"
+  has_attached_file :image, :styles => {:large => "800x600#", :medium => "600x450#", :thumb => "100x75#" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
